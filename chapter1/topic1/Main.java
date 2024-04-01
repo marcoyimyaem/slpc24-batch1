cdaa6eda94088fafbe67ae049e6c95c77af89970
marcoyimyaem <marcoyimyaem@gmail.com>
2024-04-01 06:36:37 +0000

    added Plant class and demonstrate Plant Object - OOP

--- chapter1/topic1/Main.java
+++ chapter1/topic1/Main.java
@@ -1,1 +1,16 @@
 // git init
+public class Main{
+    public static void main(String[] args) {
+        Plant plant1 = new Plant();
+        plant1.numberOfLeaves = 10;
+        plant1.numberOfStem = 2;
+        System.out.println("number of leaves:"+plant1.numberOfLeaves);
+        System.out.println("number of stem:"+plant1.numberOfStem);
+    }
+
+}
+
+class Plant{
+    int numberOfLeaves;
+    int numberOfStem;
+}

28365430f48c5d6de831f15732682edc022e2937
marcoyimyaem <marcoyimyaem@gmail.com>
2024-05-04 10:58:00 +0800

    push

--- chapter5/topic1/InheritanceL.java
+++ chapter5/topic1/InheritanceL.java
@@ -1,5 +1,14 @@
 package chapter5.topic1;
 
 public class InheritanceL {
-    
+    Animal a = new Animal();
+}
+
+// final class Animal - can not extend
+class Animal{
+
+}
+
+class Mammal extends Animal{
+
 }

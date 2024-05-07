78cce1e9153da7493e2c25bbe6002c51b10c4f1e
marcoyimyaem <marcoyimyaem@gmail.com>
2024-05-07 06:29:28 +0000

    day 32 interface

--- chapter5/topic1/InheritanceL.java
+++ chapter5/topic1/InheritanceL.java
@@ -2,6 +2,7 @@ package chapter5.topic1;
 import java.util.Objects;
 public class InheritanceL {
     public static void main(String[] args) {
+
         Aspi dog1 = new Aspi(2, "Bogat", 8, 4, 13, "Baseco st.");
     }
 }
@@ -32,19 +33,41 @@ class Canine extends Mammal{
     private int numOfTeeth;
     private int weight;
 }
-class Aspi extends Canine implments CanSwim{
+class Aspi extends Canine implements CanSwim{
     String address;
     public Aspi(int age, String name,int numberOfNips,int numOfTeeth, int weight,String address) {
         super(age,name,numberOfNips,numOfTeeth,weight);
         this.address = address;
     }
+    @Override
+    public int getSPEED() {
+        return CanSwim.SPEED+10;
+        }
+    @Override
+    public int getDEPTH() {
+        return CanSwim.DEPTH-1;
+       }
    
 }
 
-abstract interface CanSwim{
-    public static final int SPEED = 10;
-    public static final int DEPTH = 5;
-    public abstract int getSPEED();
+// abstract interface CanSwim{
+//     public static final int SPEED = 10;
+//     public static final int DEPTH = 5;
+//     public abstract int getSPEED();
+//     public abstract int getDEPTH();
+    
+// }
+
+interface CanSwim implements CanFly{
+    int SPEED = 10;
+    public static int DEPTH = 5;
+    int getSPEED();
     public abstract int getDEPTH();
     
+}
+
+interface CanFly{
+    int FLY_SPEED = 10;
+    int MAX_ALTITUDE=100;
+    int getFLY_SPEED();
 }
